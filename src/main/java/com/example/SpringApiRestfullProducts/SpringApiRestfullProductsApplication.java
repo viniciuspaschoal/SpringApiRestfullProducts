7c312473b6161c1e6f3ac4dd1a00bf9c937759a1
viniciuspaschoal <viniciuspaschoal1847@gmail.com>
2025-02-24 20:11:32 -0300

    Initial commit

---
+++ src/main/java/com/example/SpringApiRestfullProducts/SpringApiRestfullProductsApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringApiRestfullProducts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApiRestfullProductsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringApiRestfullProductsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringApiRestfullProducts/SpringApiRestfullProductsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringApiRestfullProducts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringApiRestfullProductsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
